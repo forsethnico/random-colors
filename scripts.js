0a5c4a9b82f57ff538fa28987119a4f9c40c360f
Nicole Forseth <forseth.nico@gmail.com>
2022-06-01 16:02:31 -0600

    Add color to boxes by hardcode

--- scripts.js
+++ scripts.js
@@ -1,15 +1,23 @@
-var newPalletBtn = document.querySelector('.new-palette-button');
+
+var newPaletteBtn = document.querySelector('#new-palette-button');
 
 var hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 
+var color = document.querySelectorAll(".box")
 
-newPalletBtn.addEventListener('click', randomColorPallet);
+newPaletteBtn.addEventListener('click', setPalette);
 
-function randomColorPallet() {
+function randomColor() {
   var hex = '#';
   for (var i = 0; i < 6; i++){
     hex += hexValues[(Math.floor(Math.random() * 16))];
   }
   return hex;
 }
-randomColorPallet();
+
+function setPalette() {
+  var hex = randomColor();
+  color.style.backgroundColor = hex;
+  color.innerHTML = `<p>${hex}</p>`
+  console.log('hello')
+}
