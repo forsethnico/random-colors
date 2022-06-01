a01417cdd510e30ebf7c147e4fb43930fb527948
Nick Liggett <nick.liggett@gmail.com>
2022-06-01 16:32:28 -0600

    Add JS script element

--- scripts.js
+++ scripts.js
@@ -1,15 +1,19 @@
 var newPalletBtn = document.querySelector('.new-palette-button');
+var boxes = document.querySelector('.color-boxes')
 
 var hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 
 
-newPalletBtn.addEventListener('click', randomColorPallet);
+newPalletBtn.addEventListener('click', changeColors);
+boxes.addEventListener('click', changeColors)
 
 function randomColorPallet() {
   var hex = '#';
   for (var i = 0; i < 6; i++){
     hex += hexValues[(Math.floor(Math.random() * 16))];
-  }
+  } console.log(event)
   return hex;
 }
-randomColorPallet();
+ function changeColors(){
+   console.log('Hello')
+ }
